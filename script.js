7d47c063d5f665be3e8901ad87cd8878c4c3c801
hiran <hirankotla123@gmail.com>
2025-10-16 19:08:40 +0530

    Add contact form submission alert

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple contact form alert
+document.getElementById('contactForm').addEventListener('submit', function(e){
+    e.preventDefault();
+    alert('Message sent! We will contact you soon man with pride sin of lion.');
+});
